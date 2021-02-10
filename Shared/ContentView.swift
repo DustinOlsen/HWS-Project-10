0229daa79f83d41e5c779cc891453870176fc5fd
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2021-02-09 21:46:23 -0600

    Project 10 part 1/4
    Day 49 completed

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -4,16 +4,36 @@
 //
 //  Created by Dustin Olsen on 2/9/21.
 //
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
+    
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create Account") {
+                    print("Creating Account....")
+                }
+            }
+//            .disabled(username.isEmpty || email.isEmpty)
+            .disabled(disableForm)
+        }
+    }
+    
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
